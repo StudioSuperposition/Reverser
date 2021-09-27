418853c147391edd14c648bfe4c1678cf5bfd33b
VargentSomnium <bridgesmichael86@gmail.com>
2021-09-26 21:16:10 -0500

    Attached scripts
    Attached boardController scripts to all buttons in main scene

--- ReverserProject/Assets/Scripts/boardController.cs
+++ ReverserProject/Assets/Scripts/boardController.cs
@@ -1,8 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class boardController : MonoBehaviour
 {
-    
+    public void updateBoard()
+    {
+
+    }
+    public void moveHighlight()
+    {
+
+    }
+    public void flipOwner()
+    {
+        Debug.Log("This object is " + this.gameObject);
+        /*if(this.gameObject.GetComponent<Image>().isActiveAndEnabled)
+        {
+            if(GameObject.Find("GameMaster").GetComponent<gameMasterScript>().blackTurn)
+            {
+                this.gameObject.GetComponent<Image>().color = Color.black;
+            }
+            else
+            {
+                gameObject.GetComponent<Image>().color = Color.white;
+            }
+        }*/
+    }
 }
--- ReverserProject/Assets/Scripts/gameMasterScript.cs
+++ ReverserProject/Assets/Scripts/gameMasterScript.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class gameMasterScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //if  true black's turn false white's turn
+    public bool blackTurn = true;
+
+    public void turnTick()
     {
-        
+        blackTurn = !blackTurn;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        Button[] buttons = FindObjectsOfType<Button>();
+        foreach ( Button button in buttons)
+        {
+            button.GetComponent<Image>().color = Color.green;
+        }
+
+
+
+        GameObject obj = GameObject.Find("33");
+        obj.GetComponent<Image>().color = Color.white;
+        obj = GameObject.Find("34");
+        obj.GetComponent<Image>().color = Color.black;
+        obj = GameObject.Find("43");
+        obj.GetComponent<Image>().color = Color.black;
+        obj = GameObject.Find("44");
+        obj.GetComponent<Image>().color = Color.white;
     }
 }
